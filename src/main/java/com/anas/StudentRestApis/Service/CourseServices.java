738e0b51719058c3a7f940d64508e17b2bd1c967
mohdanas86 <anasalam7739@gmail.com>
2026-04-09 18:32:23 +0530

    feat: Add methods to CourseServices interface for course retrieval, creation, update, and deletion

--- src/main/java/com/anas/StudentRestApis/Service/CourseServices.java
+++ src/main/java/com/anas/StudentRestApis/Service/CourseServices.java
@@ -1,6 +1,8 @@
 package com.anas.StudentRestApis.Service;
 
 import com.anas.StudentRestApis.Dto.CourseEntityDto;
+import com.anas.StudentRestApis.Dto.CreateCourseRequestDto;
+import com.anas.StudentRestApis.Dto.UpdateCourseRequestDto;
 
 import java.util.List;
 
@@ -9,4 +11,24 @@ public interface CourseServices {
      * Retrieves all courses
      */
     List<CourseEntityDto> getAllCourses();
+
+    /**
+     * Retrieves all course by courseId
+     */
+    CourseEntityDto getCourseByCourseId(long courseId);
+
+    /**
+     * Create a new course
+     */
+    CourseEntityDto createCourse(CreateCourseRequestDto request);
+
+    /**
+     * Update a course using courseId
+     */
+    CourseEntityDto updateCourse(long courseId, UpdateCourseRequestDto request);
+
+    /**
+     * Delete a course using courseId
+     */
+    void deleteCourseByCourseId(long courseId);
 }
