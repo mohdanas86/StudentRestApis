79819e170d412e610bed966f56228032a8ae51bd
mohdanas86 <anasalam7739@gmail.com>
2026-04-09 16:36:43 +0530

    feat: Implement CourseServicesImpl with method to fetch all courses

--- src/main/java/com/anas/StudentRestApis/Service/Impl/CourseServicesImpl.java
+++ src/main/java/com/anas/StudentRestApis/Service/Impl/CourseServicesImpl.java
@@ -1,4 +1,33 @@
 package com.anas.StudentRestApis.Service.Impl;
 
-public class CourseServicesImpl {
+import com.anas.StudentRestApis.Dto.CourseEntityDto;
+import com.anas.StudentRestApis.Entity.CourseEntity;
+import com.anas.StudentRestApis.Repository.CourseRepository;
+import com.anas.StudentRestApis.Service.CourseServices;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Service for courses data operations
+ */
+@Service
+@AllArgsConstructor
+public class CourseServicesImpl implements CourseServices{
+
+    private final CourseRepository courseRepository;
+    private final ModelMapper modelMapper;
+
+    /**
+     * Fetch all courses
+     */
+    @Override
+    public List<CourseEntityDto> getAllCourses(){
+        return courseRepository.findAll()
+                .stream()
+                .map(courseEntity -> modelMapper.map(courseEntity, CourseEntityDto.class))
+                .toList();
+    }
 }
